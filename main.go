1a88e384395a0a77c5e82420e1da96829d963311
agallentes <agallentes.dev@gmail.com>
2018-09-25 14:03:06 +0800

    Add initial testing implementation

--- main.go
+++ main.go
@@ -1,7 +1,94 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/arjaygallentes/calcuCo/mathcalc"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+var tests = []string{
+	"(1+3)*7", // 28, example from task description.
+	"1+3*7",   // 22, shows operator precedence.
+	"7",       // 7, a single literal is a valid expression.c
+	"7/3",     // eval only does integer math.
+	"7.3",     // this parses, but we disallow it in eval.
+	"7^3",     // parses, but disallowed in eval.
+	"go",      // a valid keyword, not valid in an expression.
+	"3@7",     // error message is "illegal character."
+	"",        // EOF seems a reasonable error message.
+}
+
+// func main() {
+// 	for _, exp := range tests {
+// 		if r, err := mathcalc.ParseAndEval(exp); err == nil {
+// 			fmt.Println(exp, "=", r)
+// 		} else {
+// 			fmt.Printf("%s: %v\n", exp, err)
+// 		}
+// 	}
+// }
+
+// Stores the state of the terminal before making it raw
+var regularState *terminal.State
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) > 1 {
+		input := strings.Replace(strings.Join(os.Args[1:], ""), " ", "", -1)
+		res, err := mathcalc.ParseAndEval(input)
+		if err != nil {
+			return
+		}
+		fmt.Print("\n", res)
+		return
+	}
+
+	var err error
+	regularState, err = terminal.MakeRaw(0)
+	if err != nil {
+		panic(err)
+	}
+	defer terminal.Restore(0, regularState)
+
+	term := terminal.NewTerminal(os.Stdin, "> ")
+	term.AutoCompleteCallback = handleKey
+	for {
+		text, err := term.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				// Quit without error on Ctrl^D
+				exit()
+			}
+			panic(err)
+		}
+
+		text = strings.Replace(text, " ", "", -1)
+		if text == "exit" || text == "quit" {
+			break
+		}
+
+		res, err := mathcalc.ParseAndEval(text)
+		if err != nil {
+			term.Write([]byte(fmt.Sprintln("Error: " + err.Error())))
+			continue
+		}
+		term.Write([]byte(fmt.Sprintln(res)))
+	}
+}
+
+func handleKey(line string, pos int, key rune) (newLine string, newPos int, ok bool) {
+	if key == '\x03' {
+		// Quit without error on Ctrl^C
+		exit()
+	}
+	return "", 0, false
+}
+
+func exit() {
+	terminal.Restore(0, regularState)
+	fmt.Println()
+	os.Exit(0)
 }
---
+++ mathcalc/mathcalc_test.go
@@ -0,0 +1,100 @@
+package mathcalc
+
+import (
+	"strconv"
+	"testing"
+)
+
+var exps = map[string]float64{
+	"1+1":                         2,
+	"1+2^3^2":                     513,
+	"2^(3+4)":                     128,
+	"2^(3/(1+2))":                 2,
+	"2(1+3)":                      8,
+	"2^2(1+3)":                    16,
+	"-2":                          -2,
+	"1+(-1)^2":                    2,
+	"3-5":                         -2,
+	"-3*4":                        -12,
+	"3*-4":                        -12,
+	"3*(3-(5+6)^12)*23^3-5^23":    -126476703133661843,
+	"2^3^2":                       512,
+	"-3^2":                        -9,
+	"2(1+1)4":                     16,
+	"3*abs(1-5)":                  12,
+	"ln(3^15)":                    16.47918433,
+	"sqrt(10)":                    3.16227766,
+	"abs(-3/2)":                   1.5,
+	"1+2sin(-1024)tan(acos(1))^2": 1,
+	"tan(10)cos(20)":              0.2645844,
+	"2(e^3)":                      40.1710738464,
+	"sin(pi*π)":                   -0.430301217,
+	"3π":                          9.42477796,
+}
+
+const DELTA = 0.000001
+
+func TestEvaluate(t *testing.T) {
+	for expression, expected := range exps {
+		res, err := ParseAndEval(expression)
+		if err != nil {
+			message := expression + " :failed: actual value " +
+				strconv.Itoa(res) +
+				" differs from expected value " +
+				strconv.FormatFloat(expected, 'G', -1, 64) +
+				"error: " + err.Error()
+			t.Error(message)
+		} else {
+			message := expression + " :passed: " + "Result: " + strconv.Itoa(res) + " equal to Expected " + strconv.FormatFloat(expected, 'G', -1, 64)
+			t.Error(message)
+		}
+	}
+}
+
+func TestEvaluateInvalid(t *testing.T) {
+	tests := [][]string{
+		{"/"},
+		{"1/"},
+		{"1("},
+		{")("},
+		{"(()"},
+		{"@"},
+		{"@@"},
+		{"0", "@@"},
+		{"0", "@@@"},
+		{"@@\xa6"},
+	}
+	for i, series := range tests {
+		var fail error
+		for _, expr := range series {
+			if _, err := ParseAndEval(expr); err != nil {
+				fail = err
+				break
+			}
+		}
+		if fail == nil {
+			t.Errorf("case %d: expected error, finished successfully", i)
+		}
+	}
+}
+
+func BenchmarkEvaluate(b *testing.B) {
+	tests := []string{
+		"π",
+		"1+2^3^2",
+		"2^(3+4)",
+		"2^(3/(1+2))",
+		"2^2(1+3)",
+		"1+(-1)^2",
+		"3*(3-(5+6)^12)*23^3-5^23",
+		"2^3^2",
+		"ln(3^15)",
+		"sqrt(10)",
+		"abs(-3/2)",
+		"1+2sin(-1024)tan(acos(1))^2",
+		"tan(10)cos(20)",
+	}
+	for i := 0; i < b.N; i++ {
+		ParseAndEval(tests[i%len(tests)])
+	}
+}
